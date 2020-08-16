eb33be6a0f30539731c7383f5b647b4283a58a19
poornajava <poorna.sasikuamr@gmail.com>
2020-08-16 18:01:53 +0530

    Commiting Parallel Run Changes

--- src/main/java/com/qa/ElementUtils/ChromeOptions.java
+++ src/main/java/com/qa/ElementUtils/ChromeOptions.java
@@ -2,18 +2,25 @@ package com.qa.ElementUtils;
 
 import java.util.Properties;
 
+import com.qa.hubSpot.basePage.BasePage;
+
 public class ChromeOptions {
 	
 	org.openqa.selenium.chrome.ChromeOptions co;
 	Properties prop;
+	BasePage b1;
 	
 	public ChromeOptions() {
 	
-		prop= new Properties();
+		b1= new BasePage();
+		
+		 prop=b1.init_Properties();
 	}
 	
 	public org.openqa.selenium.chrome.ChromeOptions getChromeOptions() {
 		co= new org.openqa.selenium.chrome.ChromeOptions();
+		System.out.println("here...........");
+		System.out.println(prop.getProperty("headless"));
 		co.addArguments("--incognito");
 		if(prop.getProperty("headless").equals("yes"))
 		{
